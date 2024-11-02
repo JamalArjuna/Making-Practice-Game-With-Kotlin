bff91610e8a84d2587178da7917e50a36cf7121c
JamalArjuna <arjunaridwansyah@gmail.com>
2024-11-02 23:43:47 +0700

    First Commit

---
+++ src/Main.kt
@@ -0,0 +1,22 @@
+class Player{
+var Name = String()
+var Health: Double = 0.0
+
+    constructor(BaseName : String, BaseHealth : Double) {
+
+    }
+}
+
+class Weapon{
+
+}
+
+class Arnmor{
+
+}
+
+
+
+fun main() {
+
+}
